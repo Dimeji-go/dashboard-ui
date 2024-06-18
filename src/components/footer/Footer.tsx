8779ae323bd30fe2b47bed05939e78b17257ae05
Eludini Oladimeji <dimeji.go77@gmail.com>
2024-06-18 11:16:41 +0100

    first commit

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,13 @@
+import "./footer.scss";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <span>lavadmin</span>
+      <span>© Dimeji Admin Dashboard</span>
+    </div>
+  );
+};
+
+export default Footer;
+
